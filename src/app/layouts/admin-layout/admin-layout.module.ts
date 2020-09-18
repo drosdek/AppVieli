18e1cf85b434e76b574ac9f538f47460b2e9da87
Josnei Drosdek <josneidrosdek@gmail.com>
2020-09-18 13:06:06 -0300

    Removido biblioteca desnecessaria

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -4,37 +4,13 @@ import { RouterModule } from '@angular/router';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatRippleModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatSelectModule } from '@angular/material/select';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TelaoComponent } from '../../pages/telao/telao.component';
-import { MatSliderModule } from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-
-
-
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(AdminLayoutRoutes),
-    FormsModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatRippleModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatTooltipModule,
-    MatSliderModule,
-    MatSlideToggleModule
   ],
   declarations: [
-    DashboardComponent,
-    TelaoComponent
+    DashboardComponent
   ]
 })
 export class AdminLayoutModule { }
