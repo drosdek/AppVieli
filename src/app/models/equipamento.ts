740471c0664ca94c64e118d21f6aca95c0a65aee
Josnei Drosdek <josneidrosdek@gmail.com>
2020-09-18 13:38:48 -0300

    Adicionado Modelo Equipamento

--- src/app/models/equipamento.ts
+++ src/app/models/equipamento.ts
@@ -1,4 +1,22 @@
 export interface Equipamento {
   id_equipamento: number;
-  
+  equipamento: string;
+  unidade: string;
+  horaatual: string;
+  metaminuto: number;
+  producao: number;
+  retrabalho: number;
+  metaatual: number;
+  disponibilidade: number;
+  performance: number;
+  qualidade: number;
+  pctmeta: number;
+  ritmodia: number;
+  ritmometa: number;
+  ritmominuto: null;
+  inicioparada: null;
+  TempoParado: null;
+  metaoee: number;
+  grupotelao: number;
+  pctretrabalho: number;
 }
