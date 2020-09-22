3f1901aa45a69270d7047ffa532cfaf1bff8183f
Josnei Drosdek <josneidrosdek@gmail.com>
2020-09-22 13:29:32 -0300

    Adicionado Cronometro

--- src/app/pages/controle-producao/controle-producao.component.ts
+++ src/app/pages/controle-producao/controle-producao.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Equipamento } from 'src/app/models/equipamento';
 import { EquipamentoService } from 'src/app/services/equipamento.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-controle-producao',
@@ -12,15 +13,15 @@ export class ControleProducaoComponent implements OnInit {
 
   equipamento = {} as Equipamento;
   equipamentos: Equipamento[];
+  timerResult: any;
 
-  constructor(private equipamentoService: EquipamentoService) { }
+  constructor(private equipamentoService: EquipamentoService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
-
     this.getEquipamentos();
   }
 
-  // Chama o serviço para obtém todos os carros
+  // Chama o serviço para obtém todos os equipamentos
   getEquipamentos(): void {
     this.equipamentoService.getEquipamentos().subscribe((equipamentos: Equipamento[]) => {
       this.equipamentos = equipamentos;
@@ -33,4 +34,27 @@ export class ControleProducaoComponent implements OnInit {
     form.resetForm();
     this.equipamento = {} as Equipamento;
   }
+
+  startTimer(time): any {
+    const dif: any = new Date(time);
+    console.log(dif);
+    let seconds: any = Math.floor((dif / 1000) % 60);
+    let minutes: any = Math.floor((dif / (1000 * 60)) % 60);
+    let hours: any = Math.floor((dif / (1000 * 60 * 60)) % 999);
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return this.timerResult = hours + ':' + minutes + ':' + seconds;
+    console.log(hours);
+    // setTimeout(this.startTimer, 1000);
+  }
+
+  goToTelao(id) {
+    console.log(id)
+    // this.router.navigateByUrl('telao');
+  }
+
+
 }
