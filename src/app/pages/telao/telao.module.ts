31b14c82a5cffa327144f0c1c8a823fea5a541dc
Josnei Drosdek <josneidrosdek@gmail.com>
2020-09-22 13:34:31 -0300

    Adicionado modulo individual para pagina Telao

---
+++ src/app/pages/telao/telao.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TelaoComponent } from './telao.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    TelaoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TelaoModule { }
